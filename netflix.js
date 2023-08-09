18adf9d04ade5986b50fc8896a47e5d4b251ce44
Pawankumar Shedage <pawankumarshedage@gmail.com>
2023-08-09 22:43:26 +0530

    Added my project Netflix Clone

---
+++ netflix.js
@@ -0,0 +1,73 @@
+
+function changeToCross(id){
+
+        var icon = document.getElementById(id);
+
+        //tmr add this project on repo.github.
+        //to do , unclick the cross should again turn to plus...
+        // var count = 0;
+        // count++;
+        // We can implement switch case.
+        // switch
+
+        switch(id)
+        {
+                case "plus-icon1":{
+                        if(icon.className = "fa fa-plus" && count == 1 )
+                        {
+                                icon.className = "fa fa-xmark";
+                                
+                        }
+                        else{
+                                icon.className = "fa fa-plus";
+                        }
+                        break;
+                }
+                case "plus-icon2":{
+                        if(icon.className = "fa fa-plus")
+                        {
+                                icon.className = "fa fa-xmark";
+                        }
+                        break;
+                }
+                case "plus-icon3":{
+                        if(icon.className = "fa fa-plus")
+                        {
+                                icon.className = "fa fa-xmark";
+                        }
+                        break;
+                }
+                case "plus-icon4":{
+                        if(icon.className = "fa fa-plus")
+                        {
+                                icon.className = "fa fa-xmark";
+                        }
+                        break;
+                }
+                case "plus-icon5":{
+                        if(icon.className = "fa fa-plus")
+                        {
+                                icon.className = "fa fa-xmark";
+                        }
+                        break;
+                }
+                case "plus-icon6":{
+                        if(icon.className = "fa fa-plus")
+                        {
+                                icon.className = "fa fa-xmark";
+                        }
+                        break;
+                }
+
+                default:{
+                        document.getElementById(id).style.color = "red"; 
+                }
+        }
+        
+
+        
+}
+
+
+const arr = new Set([23,35,34,64]);
+console.log(arr.values());
