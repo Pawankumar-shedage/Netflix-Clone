23e856c243bd05b72740dfb4b7989100d655f36b
Pawankumar Shedage <pawankumarshedage@gmail.com>
2023-08-13 01:36:52 +0530

    updated the js fucntion for click event of plus icon at quentions section

--- netflix.js
+++ netflix.js
@@ -1,72 +1,102 @@
 
-function changeToCross(id){
+//Noob approach
 
-        var icon = document.getElementById(id);
+// function changeToCross(id){
 
-        //tmr add this project on repo.github.
-        //to do , unclick the cross should again turn to plus...
-        // var count = 0;
-        // count++;
-        // We can implement switch case.
-        // switch
+//         var icon = document.getElementById(id);
 
-        switch(id)
-        {
-                case "plus-icon1":{
-                        if(icon.className = "fa fa-plus" && count == 1 )
-                        {
-                                icon.className = "fa fa-xmark";
+//         //tmr add this project on repo.github.
+//         //to do , unclick the cross should again turn to plus...
+//         // var count = 0;
+//         // count++;
+//         // We can implement switch case.
+//         // switch
+
+//         switch(id)
+//         {
+//                 case "plus-icon1":{
+//                         if(icon.className = "fa fa-plus" )
+//                         {
+//                                 icon.className = "fa fa-xmark";
                                 
-                        }
-                        else{
-                                icon.className = "fa fa-plus";
-                        }
-                        break;
-                }
-                case "plus-icon2":{
-                        if(icon.className = "fa fa-plus")
-                        {
-                                icon.className = "fa fa-xmark";
-                        }
-                        break;
-                }
-                case "plus-icon3":{
-                        if(icon.className = "fa fa-plus")
-                        {
-                                icon.className = "fa fa-xmark";
-                        }
-                        break;
-                }
-                case "plus-icon4":{
-                        if(icon.className = "fa fa-plus")
-                        {
-                                icon.className = "fa fa-xmark";
-                        }
-                        break;
-                }
-                case "plus-icon5":{
-                        if(icon.className = "fa fa-plus")
-                        {
-                                icon.className = "fa fa-xmark";
-                        }
-                        break;
-                }
-                case "plus-icon6":{
-                        if(icon.className = "fa fa-plus")
-                        {
-                                icon.className = "fa fa-xmark";
-                        }
-                        break;
-                }
+//                         }
+//                         else{
+//                                 icon.className = "fa fa-plus";
+//                         }
+//                         break;
+//                 }
+//                 case "plus-icon2":{
+//                         if(icon.className = "fa fa-plus")
+//                         {
+//                                 icon.className = "fa fa-xmark";
+//                         }
+//                         break;
+//                 }
+//                 case "plus-icon3":{
+//                         if(icon.className = "fa fa-plus")
+//                         {
+//                                 icon.className = "fa fa-xmark";
+//                         }
+//                         break;
+//                 }
+//                 case "plus-icon4":{
+//                         if(icon.className = "fa fa-plus")
+//                         {
+//                                 icon.className = "fa fa-xmark";
+//                         }
+//                         break;
+//                 }
+//                 case "plus-icon5":{
+//                         if(icon.className = "fa fa-plus")
+//                         {
+//                                 icon.className = "fa fa-xmark";
+//                         }
+//                         break;
+//                 }
+//                 case "plus-icon6":{
+//                         if(icon.className = "fa fa-plus")
+//                         {
+//                                 icon.className = "fa fa-xmark";
+//                         }
+//                         break;
+//                 }
 
-                default:{
-                        document.getElementById(id).style.color = "red"; 
-                }
-        }
+//                 default:{
+//                         document.getElementById(id).style.color = "red"; 
+//                 }
+//         }
         
 
         
+// }
+
+
+
+//Pr approach.
+// Questions plus->cross icon.
+const crossIcons = document.querySelectorAll(".fa-plus")
+
+//Selecting the cross-icon
+function selectIcon(e) {
+        removeIcon();
+        //changing to cross.
+        this.classList.add("fa-xmark");
 }
 
+function removeIcon()
+{
+        crossIcons.forEach(x => x.classList.remove("fa-xmark"));
+}
 
+function fun11(e)
+{
+        document.getElementById("plus-icon2").style.color ="red";
+        console.log("Again");
+}
+
+//Listen for tab icon click.
+crossIcons.forEach(x,y => x.addEventListener('click', selectIcon),
+                y.addEventListener('click', fun11));
+
+// ----------------------------------------------
 
